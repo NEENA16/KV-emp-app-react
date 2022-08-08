37afb3b6c21166430f580d03a064a9b428e2996e
Neena <neenamarria@gmail.com>
2022-08-08 14:26:48 +0530

    Added tags for delete

--- src/services/api.js
+++ src/services/api.js
@@ -2,16 +2,19 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 export const empApi = createApi({
     reducerPath: 'empApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/'}),
+    employeelist: ["EmployeeList"],
     endpoints: (builder) => ({
         getallEmployee: builder.query({
             query: ( )=>'employee',
+            providesTags:["EmployeeList"],
         }),
         getEmployee: builder.query({
             query: id =>`employee/${id}`,
         }),
         deleteEmployeeById: builder.mutation({
             query: (id) => {
-                return ({ url: `employee/${id}`, method: 'DELETE'})}
+                return ({ url: `employee/${id}`, method: 'DELETE'})},
+                invalidatesTags: ["EmployeeList"],
         }),
         createEmployee: builder.mutation({
             query: (payload) =>{
